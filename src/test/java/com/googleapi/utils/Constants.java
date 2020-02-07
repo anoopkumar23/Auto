25667ced41d0ebc22e829954dbe299fe7c1c9fd7
Anoop <anoopkumar23@gmail.com>
2020-02-07 11:03:47 +0530

    updated

---
+++ src/test/java/com/googleapi/utils/Constants.java
@@ -0,0 +1,6 @@
+package com.googleapi.utils;
+
+public class Constants {	
+	public static final String GOOGLE_API_KEY = "Google-API-Key";
+	public static final String GOOGLE_GEOCODE_ENDPOINTS = "https://maps.googleapis.com/maps/api/geocode/json";	
+}
